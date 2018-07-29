e6300cb6496f080b5d9b18d244165ab61954bba7
Dongdong <1051196347@qq.com>
2018-07-30 00:44:11 +0800

    百度语音识别+只能对话+播放MP3-无线程

--- 语音识别_智能_播报_无线程.py
+++ 语音识别_智能_播报_无线程.py
@@ -22,14 +22,6 @@ import time
 import win32con
 
 
-#百度语音合成
-import urllib.request
-import requests#导入requests库
-import urllib
-import json
-import base64
-
-
 reload(sys)
 
 #sys.setdefaultencoding( "utf-8" )
@@ -92,46 +84,6 @@ def save_wave_file(filename, data):
     wf.setframerate(SAMPLING_RATE)
     wf.writeframes(b"".join(data))
     wf.close()
-
-
-
-#百度语音合成---------------------------------------
-class BaiduRest:
-    def __init__(self, cu_id, api_key, api_secert):
-        # token认证的url
-        self.token_url = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s"
-        # 语音合成的resturl
-        self.getvoice_url = "http://tsn.baidu.com/text2audio?tex=%s&lan=zh&cuid=%s&ctp=1&tok=%s"
-        # 语音识别的resturl
-        self.upvoice_url = 'http://vop.baidu.com/server_api'
-
-        self.cu_id = cu_id
-        self.getToken(api_key, api_secert)
-        return
-
-    def getToken(self, api_key, api_secert):
-        # 1.获取token
-        token_url = self.token_url % (api_key,api_secert)
-
-        r_str = requests.get(token_url).text
-        token_data = json.loads(r_str)
-        self.token_str = token_data['access_token']
-        pass
-
-    def getVoice(self, text, filename):
-        # 2. 向Rest接口提交数据
-        get_url = self.getvoice_url % (urllib.parse.quote(text), self.cu_id, self.token_str)
-
-        voice_data = urllib.request.urlopen(get_url).read()
-        # 3.处理返回数据
-        voice_fp = open(filename,'wb+')
-        voice_fp.write(voice_data)
-        voice_fp.close()
-        pass
-
-#百度语音合成--------------------------------------
-
-
 NUM_SAMPLES = 2000# pyAudio内部缓存的块的大小
 SAMPLING_RATE = 8000# 取样频率
 LEVEL = 1500# 声音保存的阈值
@@ -144,17 +96,6 @@ stream = pa.open(format=paInt16, channels=1, rate=SAMPLING_RATE, input=True,fram
 token = get_token()#获取ｔｏｋｅｎ
 key = '35ff2856b55e4a7f9eeb86e3437e23fe'
 api = 'http://www.tuling123.com/openapi/api?key=' + key + '&info='
-
-#百度语音合成--------------------------------------
-if __name__ == "__main__":
-    # 我的api_key,供大家测试用，在实际工程中请换成自己申请的应用的key和secert
-    api_key = "SrhYKqzl3SE1URnAEuZ0FKdT" 
-    api_secert = "hGqeCkaMPb0ELMqtRGc2VjWdmjo7T89d"
-    # 初始化
-    bdr = BaiduRest("test_python", api_key, api_secert)
-    # 将字符串语音合成并保存为out.mp3
-#百度语音合成--------------------------------------
-
 while(True):
     # 读入NUM_SAMPLES个取样
     string_audio_data = stream.read(NUM_SAMPLES,False);
@@ -185,7 +126,7 @@ while(True):
         if save_count < 0:
             save_count = 0
         if save_count > 0:
-# 将要保存的数据存放到save_buffer中
+         # 将要保存的数据存放到save_buffer中
                 save_buffer.append(string_audio_data )
         else:
         # 将save_buffer中的数据写入WAV文件，WAV文件的文件名是保存的时刻
@@ -193,7 +134,7 @@ while(True):
                 if len(save_buffer) > 0 or time_flag:
                         #filename = datetime.now().strftime("%Y-%m-%d_%H_%M_%S") + ".wav"#原本是用时间做名字
                         filename = str(flag_num)+".wav"
-                        flag_num += 1
+                        flag_num = 1
                         save_wave_file(filename, save_buffer)
                         save_buffer = []
                         t = 0
@@ -213,15 +154,7 @@ while(True):
                         # 将Unicode转化为普通Python字符串："encode"
                         utf8string = unicodestring.encode("utf-8")
                         print ("科塔娜："+str(a))
-
-                         # 将字符串语音合成并保存为out.mp3
-                        bdr.getVoice(str(a), "out1.mp3")
-
-    
                         # 电脑说话
-                        #speech.say(str(a))
-
-                        os.system('out1.mp3')
-                        
+                        speech.say(str(a))
                         url = "http://tsn.baidu.com/text2audio?tex="+dic_json['text']+"&lan=zh&per=0&pit=1&spd=7&cuid=7519663&ctp=1&tok=25.41bf315625c68b3e947c49b90788532d.315360000.1798261651.282335-9120612"
                         os.system('mpg123 "%s"'%(url))
